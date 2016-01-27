a5b5692f6a153ff8d6aa2b1710d9b317b4e04d73
Tabitha <Tabitha@Tabitha>
2016-01-27 11:52:55 -0700

    1/27/2016 made some first changes

---
+++ Survival/src/survival/Survival.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package survival;
+
+/**
+ *
+ * @author Tabitha
+ */
+public class Survival {
+
+    /**
+     * @param args the command line arguments
+     * push junk
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
