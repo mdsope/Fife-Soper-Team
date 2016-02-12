45ed6a714c0287b7d756db736cc2336dce5779b0
Tabitha <Tabitha@Tabitha>
2016-02-12 12:20:51 -0700

    Merge origin/master
    Conflicts:
    	Survival/src/byui/cit260/survival/control/InventoryControl.java
    	Survival/test/byui/cit260/survival/control/InventoryControlTest.java

--- Survival/test/byui/cit260/survival/control/InventoryControlTest.java
+++ Survival/test/byui/cit260/survival/control/InventoryControlTest.java
@@ -114,65 +114,6 @@ public class InventoryControlTest {
         result = instance.getboots(radius, height);
         assertEquals(expResult, result, 0.5);
        }
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-/**
- *
- * @author Tabitha
- */
-public class InventoryControlTest {
-    
-    public InventoryControlTest() {
-    }
-
-    /**
-     * Test of getSize method, of class InventoryControl.
-     */
-    @Test
-    public void testGetSize() {
-        System.out.println("getSize");
-        System.out.println("\tTest case #1'");
-        double length = 4.0;
-        double width = 6.0;
-        InventoryControl instance = new InventoryControl();
-        double expResult = 24.0;
-        double result = instance.getSize(length, width);
-        assertEquals(expResult, result, 0.4);
-       
-        
-                System.out.println("\tTest case #2'");
-         length = -1.0;
-         width = 6.0;
-         expResult = -1.0;
-         result = instance.getSize(length, width);
-        assertEquals(expResult, result, 0.4);
-        
-            System.out.println("\tTest case #3'");
-         length = 4.0;
-         width = -1.0;
-         expResult = -1.0;
-         result = instance.getSize(length, width);
-        assertEquals(expResult, result, 0.4);
-        
-            System.out.println("\tTest case #4'");
-         length = 4.0;
-         width = 4.0;
-         expResult = -3.0;
-         result = instance.getSize(length, width);
-        assertEquals(expResult, result, 0.4);
-        
-            System.out.println("\tTest case #5'");
-         length = 8.0;
-         width = 4.0;
-         expResult = -4.0;
-         result = instance.getSize(length, width);
-        assertEquals(expResult, result, 0.4);
-    }
-
-    /**
-     * Test of getTiara method, of class InventoryControl.
-     */
     @Test
     public void testGetTiara() {
         System.out.println("getTiara");
